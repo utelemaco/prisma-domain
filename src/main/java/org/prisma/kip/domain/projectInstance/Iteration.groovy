a6b23d4605678d07f9622ea0c54fc9701adf3715
Ulisses Telemaco <ulisses.telemaco@gmail.com>
2020-02-28 01:14:05 -0500

    Including property 'estimatedFinishDate' into entity Iteration

--- src/main/java/org/prisma/kip/domain/projectInstance/Iteration.groovy
+++ src/main/java/org/prisma/kip/domain/projectInstance/Iteration.groovy
@@ -1,9 +1,13 @@
 package org.prisma.kip.domain.projectInstance
 
+import java.util.Date
+
 class Iteration extends WorkGraph {
 
 	List<IterationInterval> intervals = []
 	
 	Delivery delivery
+	
+	Date estimatedFinishDate
     
 }
